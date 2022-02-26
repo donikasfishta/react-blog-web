796cc3ee2e83457db0524909d43605cf85998fc7
donikasfishta <donika.sfishta@outlook.com>
2022-02-26 01:33:26 +0100

    enable page routing and add login+register pages

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import "./topbar.css"
 
 export default function TopBar() {
+  const user = false;
   return (
     <div className='top'>
         <div className="topLeft">
@@ -11,17 +13,40 @@ export default function TopBar() {
         </div>
         <div className="topCenter">
             <ul className="topList">
-                <li className="topListItem">HOME</li>
-                <li className="topListItem">ABOUT</li>
-                <li className="topListItem">CONTACT</li>
-                <li className="topListItem">WRITE</li>
-                <li className="topListItem">LOGOUT</li>
+                <li className="topListItem"><Link className="link" to="/">HOME</Link></li>
+                <li className="topListItem"><Link className="link" to="/">ABOUT</Link></li>
+                <li className="topListItem"><Link className="link" to="/">CONTACT</Link></li>
+                <li className="topListItem"><Link className="link" to="/write">WRITE</Link></li>
+                <li className="topListItem">
+                  {user && "LOGOUT"}
+                </li>
             </ul>
         </div>
         <div className="topRight">
-            <img
+          {
+            user ? (
+              <img
             className="topImg"
-             src="https://i0.wp.com/happyhumansmindfulness.com/wp-content/uploads/2021/03/pexels-photo-1858175-edited.jpeg?fit=867%2C868&ssl=1" alt="" />
+             src="https://i0.wp.com/happyhumansmindfulness.com/wp-content/uploads/2021/03/pexels-photo-1858175-edited.jpeg?fit=867%2C868&ssl=1" 
+             alt="" />
+
+            ): (
+              <ul className="topList">
+                <li className="topListItem">
+                <Link className="link" to="/login">
+                  LOGIN
+                  </Link>
+
+                </li>
+                <li className="topListItem">
+                <Link className="link" to="/register">
+                  REGISTER
+                  </Link>
+                </li>
+              </ul>
+            )
+          }
+            
             <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
 
         </div>
