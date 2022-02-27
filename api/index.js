292a85f1ba2a02ab71405a214d56bb79d4a2203d
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-02-27 21:00:51 +0100

    .env file, and configurate mongoose to database

--- api/index.js
+++ api/index.js
@@ -4,10 +4,24 @@ const express = require("express");
 // Creating app
 const app = express();
 
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+// Configuration for env file
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO_DB, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then(console.log("Connected to MONGODB"))
+  .catch((err) => {
+    console.log(err);
+  });
+
 // try in web browser
-app.use("/", (req, res) => {
-  console.log("Hey this is Kujtim");
-});
+app.use("/", (req, res) => {});
 
 // To use that we should listen to any port
 
