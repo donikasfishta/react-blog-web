132ed9a47eddfdad2bfd06127d7b83357cc2546d
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-02-28 21:41:23 +0100

    Creating local storage of img
    Using package multer, implemented the function to create local storage for img that user will upload

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
-
+const multer = require("multer");
 // Configuration for env file
 dotenv.config();
 
@@ -26,6 +26,25 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+// Creating storage of img
+const storage = multer.diskStorage({
+  // cb - callback Function it will take care of error
+  destination: (req, file, cb) => {
+    // second parameter our destination folder
+    cb(null, "img");
+  },
+  filename: (req, file, cb) => {
+    // This is the name that we are providing to te file that we save
+    cb(null, req.body.name);
+  },
+});
+// Uplade file
+const upload = multer({ storage: storage });
+// We use single because we going to upload one file
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
