f260fab87ee6ffbb409881fda4dfa67064f87275
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-02-28 20:58:27 +0100

    Function to delete and get all posts
    Implementing function to delete a post, to get all post and get post based on user and category

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -43,5 +43,61 @@ router.put("/:id", async (req, res) => {
     res.status(500).json(err);
   }
 });
+// Delete Posst
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    // Checks username
+    if (post.username === req.body.username) {
+      try {
+        await post.delete();
+        res.status(200).json("Post has been deleted...");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(404).json("You can delete only your post!");
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get Post
+router.get("/:id", async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//Get ALL Posts, this will help to fetch all te data to client
+router.get("/", async (req, res) => {
+  // This wil catch the url based on username and category name
+  const username = req.query.user;
+  const catName = req.query.cat;
+  try {
+    let posts;
+    if (username) {
+      posts = await Post.find({ username: username });
+    } else if (catName) {
+      posts = await Post.find({
+        categories: {
+          // This will find only categories that are in array
+          $in: [catName],
+        },
+      });
+    } else {
+      posts = await Post.find();
+    }
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
