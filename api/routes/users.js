563869257d0b3ad96b8af570014510f86a23f80e
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-02-28 02:09:05 +0100

    implementing delete and Get
    i did implement get method for id of users and delete method to delete that user

--- api/routes/users.js
+++ api/routes/users.js
@@ -1,9 +1,8 @@
 // To create router we can use express
 const router = require("express").Router();
 
-// In auth we are going to use models User
 const User = require("../models/User");
-
+const Post = require("../models/Post");
 // Library to hash our password
 const bcrypt = require("bcrypt");
 
@@ -39,5 +38,38 @@ router.put("/:id", async (req, res) => {
 });
 
 // Delete
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id) {
+    try {
+      // This grabs all the post of person that we are going to delete
+      const user = await User.findById(req.params.id);
+      try {
+        //   This delete the posts of account
+        await Post.deleteMany({ username: user.username });
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json("User has been deleted..");
+      } catch (err) {
+        // status 500 error somthing with database
+        res.status(500).json(err);
+      }
+    } catch (err) {
+      res.status(404).json("User not found!");
+    }
+  } else {
+    //  401 status you are not allowed
+    res.status(401).json("You can delete only your account");
+  }
+});
+
+//Get USer
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
