25c7d953b30ec30ec8f93ecc11b8876b6699b70b
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-03-01 00:26:34 +0100

    Fetching data and showing them
    Implementing function to fetch data from backend and show all of them in home page

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -1,37 +1,24 @@
-import "./post.css"
+import "./post.css";
 
-export default function Post() {
+export default function Post({ post }) {
   return (
     <div className="post">
-        <img
-        className="postImg"
-         src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg" width="700px"
-          alt="" />
-          <div className="postInfo">
-              <div className="postCats">
-                  <span className="postCat">Music</span>
-                  <span className="postCat">Life</span>
-
-              </div>
-              <span className="postTitle">
-                  Lorem Ipsum is simply dummy
-             </span>
-             <hr/>
-            <span className="postDate">
-                1 hours ago
-            </span>
-          </div>
-          <p className="postDesc">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-          </p>
+      {post.photo && (
+        <img className="postImg" src={post.photo} width="700px" alt="" />
+      )}
+      <div className="postInfo">
+        <div className="postCats">
+          {post.categories.map((c) => (
+            <span className="postCats">{c.name}</span>
+          ))}
+        </div>
+        <span className="postTitle">{post.title}</span>
+        <hr />
+        <span className="postDate">
+          {new Date(post.createdAt).toDateString()}
+        </span>
+      </div>
+      <p className="postDesc">{post.desc}</p>
     </div>
-  )
+  );
 }
--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,15 +1,30 @@
-import Header from "../../components/header/Header"
-import Posts from "../../components/posts/Posts"
-import Sidebar from "../../components/sidebar/Sidebar"
-import "./home.css"
+import { useEffect, useState } from "react";
+import Header from "../../components/header/Header";
+import Posts from "../../components/posts/Posts";
+import Sidebar from "../../components/sidebar/Sidebar";
+import "./home.css";
+import axios from "axios";
+
 export default function Home() {
+  //Empty array because we havent fetch any data yet
+  const [posts, setPosts] = useState([]);
+  // fetch data
+  useEffect(() => {
+    const fetchPosts = async () => {
+      // we cahnge proxy to get data from backend in package.json
+      const res = await axios.get("/posts");
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
     <>
-    <Header/>
-    <div className='home'>
-      <Posts/>
-      <Sidebar/>
-    </div>
+      <Header />
+      <div className="home">
+        <Posts posts={posts} />
+        <Sidebar />
+      </div>
     </>
-  )
+  );
 }
