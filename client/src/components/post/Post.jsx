100ae2b2e0b6c26be50073b1b55859fb9dafd2c3
Kujtim Krasniqi <36826448+Kujt@users.noreply.github.com>
2022-03-14 14:03:08 +0100

    fixing some css in tags

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -12,12 +12,7 @@ export default function Post({ post }) {
             <img className="postImg" src={PF + post.photo} alt="" />
           )}
           <div className="postInfo">
-            <div className="postCats">
-              {/* {post.categories.map((c) => (
-                <span className="postCat">{c}</span>
-              ))} */}
-              {post.categories}
-            </div>
+            <div className="postCat">{post.categories}</div>
 
             <span className="postTitle">{post.title}</span>
 
